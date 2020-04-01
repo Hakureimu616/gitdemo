f8af4c5b855333db2595d3c390f5d21af26f61c9
Hakureimu616 <albertjshederay@gmail.com>
2020-04-01 13:08:10 +0800

    import typing to improve the robustness

--- quickSort.py
+++ quickSort.py
@@ -1,4 +1,6 @@
-def quickSort(ls):
+from typing import List
+
+def quickSort(ls : List[int]) -> List[int]:
 	base = ls[len(ls) // 2]
 	l = [x for x in ls if x < base]
 	r = [x for x in ls if x > base]

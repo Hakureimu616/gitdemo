af989eb8dee2640de2ace316651238a53927cfab
Hakureimu616 <albertjshederay@gmail.com>
2020-04-01 13:03:06 +0800

    add the main function to the code

--- quickSort.py
+++ quickSort.py
@@ -3,3 +3,7 @@ def quickSort(ls):
 	l = [x for x in ls if x < base]
 	r = [x for x in ls if x > base]
 	return l + [base] + r
+
+if __name__ == '__main__':
+	ls = [9, 5, 3, 8, 1, 10, 4, 11]
+	print(quickSort(ls))
